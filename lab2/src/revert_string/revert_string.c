8b9c8260dd4dc004c58a3705ce346922824acf70
kiselekkkk <alikakis01@gmail.com>
2020-03-30 11:14:01 +0000

    Add 'revert_string' file

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -9,7 +9,7 @@ void RevertString(char *str)
     int i;
     int k = 0;
     char temp;
-    for(i = strlen(str)-1; i >= 0; i--)
+    for(i = strlen(str)-1; i >= strlen(str)/2; i--)
         {
             temp = str[i];
             str[i]=str[k];
