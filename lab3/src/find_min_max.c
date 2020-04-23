e75468a86d1ef97f6be622540e10e2ac29e07e46
kiselekkkk <alikakis01@gmail.com>
2020-04-23 11:43:31 +0000

    Changed file

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -6,9 +6,9 @@ struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
-
+  int i = begin;
   // your code here
-    for(int i = begin; i < end; i++){
+    for(i = begin; i < end; i++){
 
 	if(array[i] < min_max.min)
 		min_max.min = array[i];
