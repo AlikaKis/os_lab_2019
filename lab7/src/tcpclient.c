19e617408e47856a8a1ba5c06e3b6d09789f9107
kiselekkkk <alikakis01@gmail.com>
2020-06-06 15:11:36 +0000

    Changes done

--- lab7/src/tcpclient.c
+++ lab7/src/tcpclient.c
@@ -6,14 +6,62 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <getopt.h>
 
-#define BUFSIZE 100
+//#define BUFSIZE 100
 #define SADDR struct sockaddr
 #define SIZE sizeof(struct sockaddr_in)
 
 int main(int argc, char *argv[]) {
   int fd;
   int nread;
+  int BUFSIZE = -1;
+  int PORT = -1;
+  char IP[16] = {'\0'};;
+  while (1) {
+  int current_optind = optind ? optind : 1;
+    static struct option options[] = {{"port", required_argument, 0, 0},
+                                      {"bufsize", required_argument, 0, 0},
+                                      {"ip",required_argument,0,0},
+                                      {0, 0, 0, 0}};
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+    if (c == -1)
+        break;
+    switch (c) {
+        case 0: {
+            switch (option_index) {
+                case 0:
+                    PORT = atoi(optarg);
+                    if (!(PORT>0))
+                        return 0;
+                    break;
+                case 1:
+                    BUFSIZE = atoi(optarg);
+                    if (!(BUFSIZE>0))
+                        return 0;
+                    break;
+                case 2:
+                    strcpy(IP, optarg);
+                    if (!(strlen(IP)>0))
+                        return 0;
+                    break;
+                default:
+                    printf("Index %d is out of options\n", option_index);
+            }
+        } break;
+        case '?':
+            printf("Unknown argument\n");
+            break;
+        default:
+            fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
+  if (PORT == -1 || BUFSIZE == -1 || strlen(IP)==0) {
+    fprintf(stderr, "Using: %s --port 20001 --bufsize 4 --ip 127.0.0.1\n", argv[0]);
+    return 1;
+  }
+
   char buf[BUFSIZE];
   struct sockaddr_in servaddr;
   if (argc < 3) {
